f6654f6b05943b631e0436778f2b65fc865fb383
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-16 05:50:35 +0530

    Card Type Constant Class

---
+++ src/main/java/com/payment/dsmovieticketpayment/card/CardType.java
@@ -0,0 +1,9 @@
+package com.payment.dsmovieticketpayment.card;
+
+public class CardType {
+
+    public static final String VISA = "VISA";
+    public static final String MASTER = "MASTER";
+    public static final String AMEX = "AMEX";
+
+}
